652bad518f442899c7c8e5cecb9ee2083643804e
Edin Hidayat <edinhidayat@Edins-MacBook-Pro.local>
2023-09-25 19:44:00 +0700

    Membuat blog sendiri, tampilan awal dan login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }}</title>
+    {{-- CSS Bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    {{-- Fonts Poppins --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600&display=swap" rel="stylesheet">
+    {{-- MyCss --}}
+    <link rel="stylesheet" href="../../css/style.css">
+  </head>
+  <body>
+    {{-- HEADER dan JUDUL --}}
+    <section id="utama">
+        <div class="row">
+            <div class="col">
+                <div id="header">
+                    <div class="me-5">
+                        <a href="/login" class="btn btn-sm btn-warning">Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col d-flex justify-content-center">
+                <div class="judul text-center my-4">
+                    <h2>Welcome To My Blog</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- Akhir HEADER dan JUDUL --}}
+
+    {{-- Konten BLOG --}}
+    <div class="row">
+        <div class="col">
+            <div class="konten">
+
+                <div class="konten-kotak mb-3">
+                    <a href="#" class="text-decoration-none stretched-link lh-sm">
+                        <img src="../../img/mesjid.jpeg" class="thumb" alt="Gambar Thumbnail">
+                        <div class="konten-judul text-center">
+                            <span class="text-dark">Rukun Islam</span>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    {{-- Akhir Konten BLOG --}}
+
+    {{-- JS Bootstrap 5 --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }}</title>
+    {{-- CSS Bootstrap 5 --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    {{-- Fonts Poppins --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;900&display=swap" rel="stylesheet">
+    {{-- MyCSS --}}
+    <link rel="stylesheet" href="../../css/style.css">
+  </head>
+  <body>
+    <section id="login" class="d-flex justify-content-center align-items-center">
+        <div class="row">
+            <div class="col">
+                <div class="kotak-login">
+                    <h2>Login</h2>
+                    <hr style="border:1px solid white;">
+
+                    <form action="/login" method="post">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="username" class="form-label teks-login">Username</label>
+                            <input type="text" class="form-control" id="username" name="username" placeholder="Your Username">
+                        </div>
+                        <div class="mb-4">
+                            <label for="password" class="form-label teks-login">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="Your Password">
+                        </div>
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-login">Login</button>
+                        </div>
+                    </form>
+                    <div class="text-center">
+                        <a href="/" class="kembali"><span class="fs-6">Back to Home</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    {{-- JS Bootstrap 5 --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
